c81b8e49e1513f3a555685bf2e70272fc16066ef
devarshordex <dhrumilparikh3@gmail.com>
2024-12-05 18:17:58 +0530

    Unique_dropdown
    Unique_dropdown

---
+++ src/test/java/Automation/Dhru/Unique_Dropdown.java
@@ -0,0 +1,26 @@
+package Automation.Dhru;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Unique_Dropdown {
+
+	@Test
+	public void test() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver() ;
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("hrefIncAdt")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+	}
+	
+	
+}
