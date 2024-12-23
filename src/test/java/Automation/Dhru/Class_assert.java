a1701cb3f4dc5cc4e45943926b94ae167875dc49
devarshordex <dhrumilparikh3@gmail.com>
2024-12-23 15:57:48 +0530

    test
    test

---
+++ src/test/java/Automation/Dhru/Class_assert.java
@@ -0,0 +1,18 @@
+package Automation.Dhru;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Class_assert {
+
+	@Test
+	public void test() {
+		
+		
+		
+	}
+	
+	
+}
